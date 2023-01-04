2a9c3fac4fad9d7eaf6ef0361add52ca45b02b2b
rahul <rahul4321kumar1997@gmail.com>
2023-01-04 19:15:18 +0530

    Added AddCourseForm to add the course

--- playlist/course_video/forms.py
+++ playlist/course_video/forms.py
@@ -2,26 +2,27 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
-from .models import Contact
+from .models import Contact, Course
+from .tasks import send_mail_func
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
-        model =User
-        fields=['username','email','password1','password2']
-        
-        
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = "__all__"
-        
-        
-class StatusForm(forms.Form):
-    STATUS_CHOICES =(
-    ("Yet to Start", "Yet to Start"),
-    ("In Progress", "In Progress"),
-    ("On Hold", "On Hold"),
-    ("Completed", "Completed"),)
-    status = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,
-                                          choices=STATUS_CHOICES)
+
+
+class AddCourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ["link", "tags", "public"]
+        labels = {"link": "Video Link", "tag": "Tag", "public": "IsPublic"}
+
+    def send_email(self):
+        send_mail_func.delay()
