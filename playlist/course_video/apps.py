4e21541ebca2d58a744b5777f899c74208e71aea
rahul <rahul4321kumar1997@gmail.com>
2023-01-04 19:25:38 +0530

    Some deleted and excel file

--- playlist/course_video/apps.py
+++ playlist/course_video/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class CourseVideoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'course_video'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "course_video"
+    verbose_name = "TechHub management"
