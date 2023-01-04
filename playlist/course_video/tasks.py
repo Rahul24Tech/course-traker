fb11e8b713404629f84e9d0f0634944d941e6450
rahul <rahul4321kumar1997@gmail.com>
2023-01-04 19:22:27 +0530

    Added celery task

---
+++ playlist/course_video/tasks.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from celery import shared_task
+from django.core.mail import send_mail
+from playlist import settings
+
+
+@shared_task(bind=True)
+def send_mail_func(self):
+    """
+    This function is used to send email notification in background.
+    """
+    mail_subject = "Hye from celery"
+    message = "Yaaaa....I have Added course by celery!!"
+    to_email = "test@yopmail.com"
+    send_mail(
+        subject=mail_subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[to_email],
+        fail_silently=True,
+    )
+    return "Done"
